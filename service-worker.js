042d2d2d6896f45e1736afdeaf48e0653853df66
juliano1984 <pizzalivery.ipora@gmail.com>
2024-10-30 04:12:57 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('calculadora-temperos-cache').then(cache => {
+            return cache.addAll([
+                './index.html',
+                './manifest.json',
+                './service-worker.js',
+                
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
